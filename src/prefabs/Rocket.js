c4e35825ac7d97697cdd61c05dd8a0bae950b5c1
Johnny Choi <cchoi14@ucsc.edu>
2021-06-08 03:54:13 +0900

    added sounds

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);       //add to existing, displayList, updateList
         this.isFiring = false;          //track rocket's firing status
         this.moveSpeed = 2;             //pixels per frame
+
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update() {
@@ -27,6 +29,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring)
         {
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
 
         // if fired, move up
